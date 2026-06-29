5d54cff52dd8c72caf6fe327c7d86b200de8610d
swathu351 <swathi.u.u@kipi.ai>
2026-06-29 14:06:57 +0530

    Correct smoothiefrrot_response to smoothiefroot_response
    Fix typo in variable name and update API URL.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response)
 sf_df=st.dataframe(data = smoothiefrrot_response.json(),use_container_width=True)
